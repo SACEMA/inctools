1aca99f2919aaafb182700a9f4ecb52d6545e008
Eduard Grebe <ed@eduardgrebe.net>
2016-05-03 14:51:58 +0200

    Renamed package.

---
+++ inctools/vignettes/TestCalibration.R
@@ -0,0 +1,59 @@
+## ------------------------------------------------------------------------
+library(abie)
+
+## ------------------------------------------------------------------------
+exampledata <- read.csv("../data/exampledata_testcalibration.csv")
+
+## ---- fig.width=6.5, fig.height=5, fig.align="center", fig.show='hold'----
+mdrical(data=exampledata,
+                 subid_var = "SubjectID",
+                 time_var = "DaysSinceEDDI",
+                 recency_cutoff_time = 730.5,
+                 inclusion_time_threshold = 800,
+                 functional_forms = c("cloglog_linear"),
+                 recency_rule = "binary_data",
+                 recency_vars = "Recent",
+                 n_bootstraps = 100,
+                 alpha = 0.05,
+                 plot = TRUE)
+
+## ---- fig.width=6.5, fig.height=5, fig.align="center", fig.show='hold'----
+mdrical(data=exampledata,
+                 subid_var = "SubjectID",
+                 time_var = "DaysSinceEDDI",
+                 recency_cutoff_time = 730.5,
+                 inclusion_time_threshold = 800,
+                 functional_forms = c("logit_cubic","cloglog_linear"),
+                 recency_rule = "independent_thresholds",
+                 recency_vars = c("Result","VL"),
+                 recency_params = c(10,0,1000,1),
+                 n_bootstraps = 100,
+                 alpha = 0.05,
+                 plot = TRUE)
+
+## ---- fig.width=7, fig.height=5, fig.align="center"----------------------
+mdrical(data=exampledata,
+                 subid_var = "SubjectID",
+                 time_var = "DaysSinceEDDI",
+                 recency_cutoff_time = 730.5,
+                 inclusion_time_threshold = 800,
+                 functional_forms = c("logit_cubic","cloglog_linear"),
+                 recency_rule = "independent_thresholds",
+                 recency_vars = c("Result","VL"),
+                 recency_params = c(10,0,1000,1),
+                 n_bootstraps = 100,
+                 alpha = 0.05,
+                 plot = TRUE,
+                 parallel = TRUE,
+                 cores=4)
+
+## ------------------------------------------------------------------------
+frrcal(data=exampledata,
+             subid_var = "SubjectID",
+             time_var = "DaysSinceEDDI",
+             recency_cutoff_time = 730.5,
+             recency_rule = "independent_thresholds",
+             recency_vars = c("Result","VL"),
+             recency_params = c(10,0,1000,1),
+             alpha = 0.05)
+
