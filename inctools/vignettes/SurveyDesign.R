93e08cd52e061cfce469895f44fe3dde160f0928
Eduard Grebe <ed@eduardgrebe.net>
2016-05-05 13:28:59 +0200

    Built package.

--- inctools/vignettes/SurveyDesign.R
+++ inctools/vignettes/SurveyDesign.R
@@ -2,28 +2,28 @@
 library("inctools")
 
 ## ---- echo=TRUE----------------------------------------------------------
-sspower(I1 = 0.05, I2 = 0.03, PrevH1 = 0.20, PrevH2 = 0.20, n1 = 5000, n2 = 5000, alpha = 0.05, 
+power(I1 = 0.05, I2 = 0.03, PrevH1 = 0.20, PrevH2 = 0.20, n1 = 5000, n2 = 5000, alpha = 0.05, 
         Power = "out", CR = 1, DE_H = 1, DE_R = 1, BMest = "same.test", MDRI = 200, 
         RSE_MDRI = 0.05, FRR = 0.01, RSE_FRR = 0.20, BigT = 730)
 
 ## ---- echo=TRUE----------------------------------------------------------
-sspower(I1 = 0.05, I2 = 0.03, PrevH1 = 0.20, PrevH2 = 0.15, n1 = "both", n2 = "both", alpha = 0.05, 
+power(I1 = 0.05, I2 = 0.03, PrevH1 = 0.20, PrevH2 = 0.15, n1 = "both", n2 = "both", alpha = 0.05, 
         Power = 0.8, SS = "out", CR = 1, DE_H = 1, DE_R = 1, BMest = "FRR.indep", MDRI = 200, 
         RSE_MDRI = 0.05, FRR = c(0.01,0.009), RSE_FRR = c(0.20,0.22), BigT = 730)
 
 ## ---- echo=TRUE----------------------------------------------------------
-sspower(I1 = 0.05, I2 = 0.03, PrevH1 = 0.20, PrevH2 = 0.15, n1 = 5000, n2 = "out", alpha = 0.05, 
+power(I1 = 0.05, I2 = 0.03, PrevH1 = 0.20, PrevH2 = 0.15, n1 = 5000, n2 = "out", alpha = 0.05, 
         Power = 0.8, SS = "out", CR = 1, DE_H = 1, DE_R = 1, BMest = "same.test", MDRI = 200, 
         RSE_MDRI = 0.05, FRR = 0.01, RSE_FRR = 0.21, BigT = 730)
 
 ## ---- echo=TRUE----------------------------------------------------------
-ssprecision(I = 0.015, RSE_I = 0.25, PrevH = 0.2, CR = 1, MDRI = 200, RSE_MDRI = 0.05, FRR = 0.01, 
+precision(I = 0.015, RSE_I = 0.25, PrevH = 0.2, CR = 1, MDRI = 200, RSE_MDRI = 0.05, FRR = 0.01, 
             RSE_FRR = 0.2, BigT = 730, DE_H = 1.1, DE_R = 1, n = 'out')
 
 ## ---- echo=TRUE----------------------------------------------------------
-ssprecision(I = c(0.015,0.02), RSE_I = 0.25, PrevH = c(0.10,0.20), CR = 1, MDRI = 200, RSE_MDRI = 0.05, FRR = 0.01, RSE_FRR = 0.2, BigT = 700, DE_H = 1, DE_R = 1, n = 'out', step = 3)
+precision(I = c(0.015,0.02), RSE_I = 0.25, PrevH = c(0.10,0.20), CR = 1, MDRI = 200, RSE_MDRI = 0.05, FRR = 0.01, RSE_FRR = 0.2, BigT = 700, DE_H = 1, DE_R = 1, n = 'out', step = 3)
 
 ## ---- echo=TRUE----------------------------------------------------------
-ssprecision(I = 0.017, RSE_I = 'out', PrevH = c(0.10,0.20), CR = 1, MDRI = 211, RSE_MDRI = 0.05, 
+precision(I = 0.017, RSE_I = 'out', PrevH = c(0.10,0.20), CR = 1, MDRI = 211, RSE_MDRI = 0.05, 
             FRR = 0.009, RSE_FRR = 0.2, BigT = 720, n = 5000, step = 5)
 
