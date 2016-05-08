3f1c5f5767a2a3f62a3a99fa62a34acf60fc9346
Avery McIntosh <avery.i.mcintosh@gmail.com>
2016-05-08 17:13:46 -0400

    made naming parallel in inctools.R documentation

--- inctools/R/inctools.R
+++ inctools/R/inctools.R
@@ -13,10 +13,10 @@
 #' inctools: a package for incidence estimation
 #'
 #' The inctools package uses methods described by Kassanjee, et al. 'A new general biomarker-based incidence estimator,' \emph{Epidemiology} (2012), to implement functions to calculate incidence and tests of incidence difference between two populations, as well as power and sample size constraints for differenct study scenarios.
-#' abie also provides functions for calculation of mean duration of recent infection and false recency rates from assays for recent infection.
+#' inctools also provides functions for calculation of mean duration of recent infection and false recency rates from assays for recent infection.
 #'
 #' @section inctools Functions:
-#' inctools has functions \emph{frrcal} to estimate false recency rate; \emph{mdrical} to estimate mean duration or recent infection; \emph{incprops} and \emph{inccounts} to calculate estimates and confidence intervals for incidence and incidence difference, as well as other summary and inferential statistics related to the survey; \emph{power} to calculate sample size needed for a given power in a test of incidence difference, or vice versa; and \emph{precision}, which gives sample size needed for a given precision in the incidence estimate or vice versa.
+#' inctools has functions \emph{frrcal} to estimate false recency rate; \emph{mdrical} to estimate mean duration or recent infection; \emph{incprops} and \emph{inccounts} to calculate estimates and confidence intervals for incidence and incidence difference, as well as other summary and inferential statistics related to the survey; \emph{incpower} to calculate sample size needed for a given power in a test of incidence difference, or vice versa; and \emph{incprecision}, which gives sample size needed for a given precision in the incidence estimate or vice versa.
 #'
 #' For a longer introduction, see the introductory vignette for this package. Use vignette('Introduction',package='inctools') to access the document.
 #'
