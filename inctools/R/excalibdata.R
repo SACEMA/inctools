a84bcc33ee5379cac4383372e5c84928d2e0a1a1
Eduard Grebe <ed@eduardgrebe.net>
2016-05-09 11:04:06 +0200

    Reformated and documented dataset.

---
+++ inctools/R/excalibdata.R
@@ -0,0 +1,7 @@
+#' The dataset `excalibdata.Rdata` contains example data from an evaluation of
+#' an assay measuring recency of infection. At an assay result of <10, the
+#' specimen is considered to be recently infected. It further contains viral
+#' load data, which is commonly used to reduce false recency. For example,
+#' when recency is defined as assay result <10 and viral load > 1000, the FRR
+#' is substantially lower (but the MDRI is also reduced).
+"excalibdata"
