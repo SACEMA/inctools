4fb03ce868a632c1bc2ad32e13c65665a5876189
Eduard Grebe <eduardgrebe@sun.ac.za>
2017-04-13 12:08:35 +0200

    Added reference to description and incremented version for CRAN submission

--- test_doSNOW.R
+++ test_doSNOW.R
@@ -1,5 +1,6 @@
-
-mdrical(data=excalibdata,
+library(inctools)
+# No parallelisation
+system.time(print(mdrical(data=excalibdata,
         subid_var = "SubjectID",
         time_var = "DaysSinceEDDI",
         recency_cutoff_time = 730.5,
@@ -8,11 +9,15 @@ mdrical(data=excalibdata,
         recency_rule = "independent_thresholds",
         recency_vars = c("Result","VL"),
         recency_params = c(10,0,1000,1),
-        n_bootstraps = 1000,
+        n_bootstraps = 10000,
         alpha = 0.05,
         plot = FALSE,
-        parallel = FALSE)
-mdrical(data=excalibdata,
+        parallel = FALSE)))
+# inctools 1.0.7: time: 982.879 SD:
+# inctools 1.0.7.9100:
+
+# With parallelisation
+system.time(print(mdrical(data=excalibdata,
         subid_var = "SubjectID",
         time_var = "DaysSinceEDDI",
         recency_cutoff_time = 730.5,
@@ -25,8 +30,9 @@ mdrical(data=excalibdata,
         alpha = 0.05,
         plot = FALSE,
         parallel = TRUE,
-        cores = 4)
-
+        cores = 4)))
+# inctools 1.0.7: time: 522.374 PE: 259.2234 SD: 14.5339
+# inctools 1.0.7.9200: 48.630 PE: 259.2234 SD: 14.5723
 
 # Try the following with versions of the package using doMC and doSNOW
 
